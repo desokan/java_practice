4db780e6a1b1a0ac1303a313a41d4c304764fce6
desokan <thefdmm@gmail.com>
2024-04-05 16:22:59 +0100

    added an if statement

--- 04_intellij_basics/HelloWorld/src/Hello.java
+++ 04_intellij_basics/HelloWorld/src/Hello.java
@@ -1,8 +1,11 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Tim");
-        System.out.print("Hello World");
-
+        System.out.println("Hello, Rasheed");
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
--- 04_intellij_basics/HelloWorld/src/SecondClass.java
+++ 04_intellij_basics/HelloWorld/src/SecondClass.java
@@ -1,5 +1,5 @@
 public class SecondClass {
     public static void main(String[] args) {
-        System.out.print("Hello, Tim");
+        System.out.print("Hello, Hakeem");
     }
 }
