50e2fa07c0cabcde86bdf22baa70288b1d130e89
desokan <thefdmm@gmail.com>
2024-04-07 15:30:13 +0100

    added new dog of array type

--- 04_intellij_basics/HelloWorld/src/Dog.java
+++ 04_intellij_basics/HelloWorld/src/Dog.java
@@ -7,3 +7,38 @@ class Dog {
         System.out.println("Woof Woof!");
     }
 }
+
+class Dog2 {
+    String name;
+    public static void main(String[] args) {
+        // make a Dog object and access it
+        Dog2 dog1 = new Dog2();
+        dog1.bark();
+        dog1.name = "Bart";
+        // now make a Dog array
+        Dog2[] myDogs = new Dog2[3];
+        // and put some dogs in it
+        myDogs[0] = new Dog2();
+        myDogs[1] = new Dog2();
+        myDogs[2] = dog1;
+        // now access the Dogs using the array references
+        myDogs[0].name = "Fred";
+        myDogs[1].name = "Marge";
+        // What is myDogs[2] name?
+        System.out.print("last dog’s name is ");
+        System.out.println(myDogs[2].name);
+        // now loop through the array and tell all dogs to bark
+        int x = 0;
+        while (x < myDogs.length) {
+            myDogs[x].bark();
+            x = x + 1;
+        }
+    }
+    public void bark() {
+        System.out.println(name + " says Ruff!");
+    }
+
+    public void eat() {}
+
+    public void chaseCat() {}
+}
--- 04_intellij_basics/HelloWorld/src/DogTestDrive.java
+++ 04_intellij_basics/HelloWorld/src/DogTestDrive.java
@@ -1,7 +1,8 @@
 public class DogTestDrive {
     public static void main(String[] args) {
-        Dog d = new Dog();
-        d.size = 33;
+        Dog d;
+        d = new Dog();
+        d.size = 99;
         d.bark();
         System.out.println(d.size);
     }
